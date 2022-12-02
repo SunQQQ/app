d430cdb22dffb0ce92becfb613db4cfc044904d5
sunquan <1585437938@qq.com>
2022-12-02 11:21:42 +0800

    学习$.extend()

--- src/components/$extend/index.js
+++ src/components/$extend/index.js
@@ -2,6 +2,7 @@ import $ from "jQuery"
 
 function $extend(){
     console.log("jq 深度复制和深度合并");
+    console.log("extend方法定义：$.extend( target [, object1 ] [, objectN ] )");
 
     let person = {
         name: 'sq',
@@ -30,7 +31,8 @@ function $extend(){
     // $.extend注意点：
     // 1、第一个参数为true时，深度拷贝。比如第三个对象的属性值也是对象，这时第二个对象的同名对象属性也会合并。否则上述情况会被覆盖
     // 2、第一个对象会被修改，且与$.extend()返回数据一致；如果不想被篡改，可以将第一个对象赋值{}
-    // 3、第一个参数true,用于深度合并
+    // 3、如果只指定1个参数，用法是不对的。此时意味着参数target被省略，target就是jQuery对象本身。通过这种方式，我们可以为全局对象jQuery添加新的函数。
+    // 4、第一个参数true,用于深度合并
 }
 
 export default $extend;
